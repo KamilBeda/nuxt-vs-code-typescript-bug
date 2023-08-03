dcfad990e8b8faf25b63a620ee6502e6dd860a9f
Kamil Beda <kamilbeda1@gmail.com>
2023-08-03 03:28:58 +0200

    Init

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+function doSomething() {
+  console.log("hello");
+}
+
+const images = ref([]);
+</script>
+
+<template>
+  <WithSlots>
+    Hello
+    <template v-if="images.length === 0" #subtitle>
+      <span>World</span>
+    </template>
+    <template v-else-if="images.length === 1" #caption>
+      <span @click="doSomething">Caption</span>
+    </template>
+    <template v-else #caption>
+      <span @click="doSomething">Caption</span>
+    </template>
+  </WithSlots>
+</template>
---
+++ components/WithSlots.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>
+      <slot />
+    </h1>
+    <h2>
+      <slot name="subtitle" />
+    </h2>
+    <h3>
+      <slot name="caption" />
+    </h3>
+  </div>
+</template>
